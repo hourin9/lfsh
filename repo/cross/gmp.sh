ae524322844c83dd4d3faeb4818c7e6cda3fa669
hourin <phucjaki@gmail.com>
2026-01-20 17:30:56 +0700

    Added: GMP (only extracts source)

---
+++ repo/cross/gmp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+NAME=gmp
+VERSION=6.3.0
+URL="https://ftp.gnu.org/gnu/gmp/gmp-$VERSION.tar.xz"
+
+unpack() {
+    tar xf "$NAME-$VERSION.tar.xz"
+}
+
+pkg_build() {
+    :
+}
+
+pkg_install() {
+    :
+}
+
