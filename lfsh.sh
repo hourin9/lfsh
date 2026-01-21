c93eadcdd45c9080c7af91853c09e54046cac0af
hourin <phucjaki@gmail.com>
2026-01-21 17:33:58 +0700

    Added: mount_vfs()

--- lfsh.sh
+++ lfsh.sh
@@ -71,5 +71,25 @@ check() {
     exit 0
 }
 
+mount_vfs() {
+    if [ -z "$LFS" ]; then
+        exit 1
+    fi
+
+    mkdir -pv $LFS/{dev,proc,sys,run}
+
+    mount -v --bind /dev $LFS/dev
+    mount -vt devpts devpts -o gid=5,mode=0620 $LFS/dev/pts
+    mount -vt proc proc $LFS/proc
+    mount -vt sysfs sysfs $LFS/sys
+    mount -vt tmpfs tmpfs $LFS/run
+
+    if [ -h $LFS/dev/shm ]; then
+        install -v -d -m 1777 $LFS$(realpath /dev/shm)
+    else
+        mount -vt tmpfs -o nosuid,nodev tmpfs $LFS/dev/shm
+    fi
+}
+
 func=$1; shift; $func "$@"
 
