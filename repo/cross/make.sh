b167029a7d03c7c31c5cd13487c8bd906418d814
hourin <phucjaki@gmail.com>
2026-01-21 11:52:29 +0700

    Added: cross/make

---
+++ repo/cross/make.sh
@@ -0,0 +1,25 @@
+NAME=make
+VERSION=4.4.1
+URL="https://ftp.gnu.org/gnu/$NAME/$NAME-$VERSION.tar.gz"
+
+unpack() {
+    tar xf "$NAME-$VERSION.tar.gz"
+}
+
+pkg_build() {
+    cd $NAME-$VERSION
+
+    mkdir -pv build && cd build
+    ../configure \
+        --prefix=/usr \
+        --host=$LFS_TGT \
+        --build=$(../build-aux/config.guess)
+
+    make
+}
+
+pkg_install() {
+    cd "$NAME-$VERSION/build"
+    make DESTDIR=$LFS install
+}
+
