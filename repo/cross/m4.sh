3a7a8542fa4c7b3bf84d05e3f2ca51b801cd6db1
hourin <phucjaki@gmail.com>
2026-01-21 09:30:25 +0700

    Added: cross/m4

---
+++ repo/cross/m4.sh
@@ -0,0 +1,24 @@
+NAME=m4
+VERSION=1.4.20
+URL="https://ftp.gnu.org/gnu/$NAME/$NAME-$VERSION.tar.xz"
+
+unpack() {
+    tar xf "$NAME-$VERSION.tar.xz"
+}
+
+pkg_build() {
+    cd $NAME-$VERSION
+
+    mkdir -pv build && cd build
+    ../configure --prefix=/usr \
+        --host=$LFS_TGT \
+        --build=$(../build-aux/config.guess)
+
+    make
+}
+
+pkg_install() {
+    cd "$NAME-$VERSION/build"
+    make DESTDIR=$LFS install
+}
+
